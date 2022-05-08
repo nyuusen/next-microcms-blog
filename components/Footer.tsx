0120747378cd5c2aa0ead99ac1cdd95fb4719964
tom-illy <pppike.0131@gmail.com>
2022-05-08 11:37:49 +0900

    add Footer.tsx

---
+++ components/Footer.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+const Footer = () => {
+  return (
+    <footer className="text-gray-600 body-font">
+      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        footerです
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
