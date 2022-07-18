5860b4c760f0fba006f5e68e1102145b73cb9872
tom-illy <pppike.0131@gmail.com>
2022-07-18 15:13:40 +0900

    ts化 & refactor

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -1,27 +1,47 @@
+import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
+import { MicroCMSListContent, MicroCMSListResponse } from 'microcms-js-sdk';
 import { client } from 'libs/client';
+import { Article } from 'types/article';
+import { ParsedUrlQuery } from 'querystring';
 
-const Article = () => {
+type Props = MicroCMSListContent & Article;
+interface Params extends ParsedUrlQuery {
+  id: string;
+}
+
+const Article: NextPage<Props> = props => {
+  console.log(props);
   return (
     <>
+      <h2>{props.title}</h2>
       <div>詳細ページです</div>
     </>
   );
 };
 
-export const getStaticPaths = async () => {
-  const data = await client.get({ endpoint: 'blogs' });
+export const getStaticPaths: GetStaticPaths<Params> = async () => {
+  const microCmsArticlesResponse = await client.get<
+    MicroCMSListResponse<Article>
+  >({
+    endpoint: 'blogs',
+  });
 
-  const paths = data.contents.map(content => `/${content.id}`);
+  const paths = microCmsArticlesResponse.contents.map(({ id }) => `/${id}`);
   return { paths, fallback: false };
 };
 
-export const getStaticProps = async context => {
-  const id = context.params.id;
-  const data = await client.get({ endpoint: 'blogs', contentId: id });
-
+export const getStaticProps: GetStaticProps<Props, Params> = async ({
+  params,
+}) => {
+  const microCmsArticleResponse = await client.get<
+    MicroCMSListContent & Article
+  >({
+    endpoint: 'blogs',
+    contentId: params.id,
+  });
   return {
     props: {
-      blog: data,
+      ...microCmsArticleResponse,
     },
   };
 };
